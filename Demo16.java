a89a91aa9d0c33e97131a7901b38391d03e5aea0
Shourya1224 <shourya21m@gmail.com>
2025-07-07 22:34:32 +0530

    hello

---
+++ Demo16.java
@@ -0,0 +1,14 @@
+
+
+ public class Demo16 {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= 10);
+    }
+}
+    
+
