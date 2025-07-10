1b4bfd5e263ad66f93279f3c184592a1379f2ecf
Shourya1224 <shourya21m@gmail.com>
2025-07-10 23:35:55 +0530

    Add files via upload

---
+++ Demo18.java
@@ -0,0 +1,29 @@
+
+    public class Demo18{
+
+
+    public static void main(String[] args)
+    {
+        int n = 10;
+        int m = 0;
+
+        try {
+          
+           
+            int ans = n / m;
+            System.out.println("Answer: " + ans);
+        }
+        catch (ArithmeticException e) {
+          
+        
+            System.out.println(
+                "Error: Division by zero is not allowed!");
+        }
+        finally {
+            System.out.println(
+                "Program continues after handling the exception.");
+        }
+    }
+}
+    
+
